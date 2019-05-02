90c08edd2b8bc10f39948712874861592a05b125
DrMantaRay <patrick.chen64@gmail.com>
2019-05-01 21:26:19 -0700

    Adding Renderer

--- EndlessMaze/EndlessMaze/EndlessMazeGame.cpp
+++ EndlessMaze/EndlessMaze/EndlessMazeGame.cpp
@@ -8,23 +8,49 @@
 EndlessMazeGame::EndlessMazeGame(): _window_(nullptr),_screenSurface_(nullptr) {
 }
 
-int EndlessMazeGame::run() {
+bool EndlessMazeGame::init() {
+
+	bool success = true;
+
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-		std::cout << "SDL could not initialize! SDL_Error: %s\n" << SDL_GetError() << std::endl;
+		std::cerr << "SDL could not initialize! SDL_Error: %s\n" << SDL_GetError() << std::endl;
+		success = false;
 	}
 	else {
+		
+		if (!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1")) {
+			std::cerr << "Warning: Linear texture filtering not enabled" << std::endl;
+		}
+
 		_window_.reset(SDL_CreateWindow("Endless Maze", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
 			SCREEN_HEIGHT, SDL_WINDOW_SHOWN));
 		if (_window_ == nullptr) {
-			printf("Window couldn't be created. SDL_Error: %s\n", SDL_GetError());
+			std::cerr << "Window couldn't be created. SDL_Error: %s\n" << SDL_GetError() << std::endl;
+			success = false;
 		}
 		else {
-			_screenSurface_.reset(SDL_GetWindowSurface(_window_.get()));
-			SDL_FillRect(_screenSurface_.get(), nullptr, SDL_MapRGB(_screenSurface_->format, 0xFF, 0xFF, 0xFF));
-			SDL_UpdateWindowSurface(_window_.get());
-			SDL_Delay(2000);
+			_screenRenderer_.reset(SDL_CreateRenderer(_window_.get(), -1, SDL_RENDERER_ACCELERATED));
+			if (_screenRenderer_ == nullptr) {
+				std::cerr << "Renderer could not be created! SDL Error" << SDL_GetError() << std::endl;
+				success = false;
+			}
+			else {
+				SDL_SetRenderDrawColor(_screenRenderer_.get(), 0xFF, 0xFF, 0xFF, 0xFF);
+			}
 		}
-		SDL_Quit();
 	}
-	return 0;
+	return success;
+}
+
+bool EndlessMazeGame::run() {
+	if (!init()) {
+		std::cerr << "Failed to initialize!" << std::endl;
+	}
+	else {
+
+	}
+}
+
+bool EndlessMazeGame::close() {
+	SDL_Quit();
 }
--- EndlessMaze/EndlessMaze/Header.h
+++ EndlessMaze/EndlessMaze/Header.h
@@ -8,25 +8,49 @@ const int SCREEN_HEIGHT = 480;
 struct WindowDeleter;
 struct SurfaceDeleter;
 
+enum KeyPressSurfaces {
+	KEY_PRESS_SURFACE_DEFAULT,
+	KEY_PRESS_SURFACE_UP,
+	KEY_PRESS_SURFACE_DOWN,
+	KEY_PRESS_SURFACE_LEFT,
+	KEY_PRESS_SURFACE_RIGHT,
+	KEY_PRESS_SURFACE_TOTAL
+};
+
 struct WindowDeleter {
-	void operator()(SDL_Window * sdl_window) const {
+	void operator()(SDL_Window * sdl_window) {
 		SDL_DestroyWindow(sdl_window);
 	}
 };
 
 struct SurfaceDeleter {
-	void operator()(SDL_Surface * sdl_surface) const {
+	void operator()(SDL_Surface * sdl_surface) {
 		SDL_FreeSurface(sdl_surface);
 	}
 };
 
+struct RendererDeleter {
+	void operator()(SDL_Renderer * sdl_renderer) {
+		SDL_DestroyRenderer(sdl_renderer);
+	}
+};
+
+struct TextureDeleter {
+	void operator()(SDL_Texture * sdl_texture) {
+		SDL_DestroyTexture(sdl_texture);
+	}
+};
 class EndlessMazeGame {
 	public:
 		EndlessMazeGame();
-		int run();
+		bool run();
+		bool init();
+		bool loadMedia();
+		bool close();
 	private:
 		std::unique_ptr<SDL_Window, WindowDeleter> _window_;
 		std::unique_ptr<SDL_Surface, SurfaceDeleter> _screenSurface_;
+		std::unique_ptr<SDL_Renderer, RendererDeleter> _screenRenderer_;
 
 };
 
